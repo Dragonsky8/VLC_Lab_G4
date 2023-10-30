89d8eb07f1deaf82c8d640ed3ec291523c30bdd4
zlou <z.lou@student.tudelft.nl>
2023-10-30 22:17:26 +0800

    apply dwt, the data is recorded by moving hand from left to right

--- Lab3/solar_cell/solar_cell.ino
+++ Lab3/solar_cell/solar_cell.ino
@@ -1,12 +1,15 @@
 #include <vector>
 using namespace std;
-const int ldrPin_1 = A0;
+const int ldrPin_1 = A7;
 const int ldrPin_2 = A2;
 const int ldrPin_3 = A4;
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200); // Set the Baud rate to 115200 bits/s
+  pinMode(A7, INPUT);
+  pinMode(A2, INPUT);
+  pinMode(A4, INPUT);
 }
 
 
@@ -17,15 +20,15 @@ void loop() {
   int ldrValue_2 = analogRead(ldrPin_2);
   int ldrValue_3 = analogRead(ldrPin_3);
 
-  Serial.print(ldrPin_1);
+  Serial.print(ldrValue_1);
   Serial.print(" ");
-  Serial.print(ldrPin_2);
+  Serial.print(ldrValue_2);
   Serial.print(" ");
-  Serial.println(ldrPin_3);
+  Serial.println(ldrValue_3);
   // cell_1.push_back(ldrValue_1);
   // for (int i = 0; i < cell_1.size(); i++){
   //   Serial.println(cell_1[i]);
   //   delay(100);
   // }
-  delay(100);
+  delay(100); //frequency 10Hz
 }
