dfb238beab2a9e4da763c3786d465c34b0b479cc
zlou <z.lou@student.tudelft.nl>
2023-11-01 00:29:41 +0800

    need to feature detection now

--- Lab3/solar_cell/solar_cell.ino
+++ Lab3/solar_cell/solar_cell.ino
@@ -16,22 +16,22 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  int ldrValue_1 = analogRead(A7); // read the value of LDR
+  // int ldrValue_1 = analogRead(A7); // read the value of LDR
 
-  int ldrValue_2 = analogRead(A2);
+  // int ldrValue_2 = analogRead(A2);
  
   int ldrValue_3 = analogRead(A4);
-    
 
-  Serial.print(ldrValue_1);
-  Serial.print(" ");
-  Serial.print(ldrValue_2);
-  Serial.print(" ");
+
+  // Serial.print(ldrValue_1);
+  // Serial.print(" ");
+  // Serial.print(ldrValue_2);
+  // Serial.print(" ");
   Serial.println(ldrValue_3);
   // cell_1.push_back(ldrValue_1);
   // for (int i = 0; i < cell_1.size(); i++){
   //   Serial.println(cell_1[i]);
   //   delay(100);
   // }
-  delay(50); //frequency 500Hz
+  delay(2); //frequency 500Hz
 }
