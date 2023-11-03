a33752da89ebbf84844fd82b9b767c86e8860dd9
zlou <z.lou@student.tudelft.nl>
2023-11-03 15:52:46 +0800

    use two sensors

--- Lab3/solar_cell/solar_cell.ino
+++ Lab3/solar_cell/solar_cell.ino
@@ -18,15 +18,15 @@ void loop() {
   // put your main code here, to run repeatedly:
   // int ldrValue_1 = analogRead(A7); // read the value of LDR
 
-  // int ldrValue_2 = analogRead(A2);
+  int ldrValue_2 = analogRead(A2);
  
   int ldrValue_3 = analogRead(A4);
 
 
   // Serial.print(ldrValue_1);
   // Serial.print(" ");
-  // Serial.print(ldrValue_2);
-  // Serial.print(" ");
+  Serial.print(ldrValue_2);
+  Serial.print(" ");
   Serial.println(ldrValue_3);
   // cell_1.push_back(ldrValue_1);
   // for (int i = 0; i < cell_1.size(); i++){
